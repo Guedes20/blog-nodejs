f98d5e532a0142644fbc2cc44e3701b6b2ddae20
Fernando Guedes <fernandoguedes20@gmail.com>
2019-12-02 18:46:03 -0500

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-// Carregar o dotenv
-//require('dotenv').config();
-
-//Carregando modulos
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
-const admin = require('./routes/admin');
-const path = require('path');
-const flash = require('connect-flash');
-
+const mongoose = require("mongoose");
+const admin = require("./routes/admin");
+const path = require("path");
+const session = require("express-session")
+const flash = require("connect-flash");
 
 //Configurações
+   //Sessao
+     app.use(session({
+        secret:"cursodenode",
+        resave:true,
+        saveUninitialized:true
+     }))
+     
+//Flash
+app.use(flash())
+
+//Middleware
+app.use((req, res, next) => {
+   res.locals.success_msg = req.flash("sucess_msg")
+   res.locals.error_msg = req.flash("error_msg")
+   next()
+})
+
 //bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,7 +35,7 @@ app.set('view engine', 'handlebars');
 
 //Mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/blogapp").then(() => {
+mongoose.connect('mongodb://localhost/blogapps' , { useNewUrlParser: true }).then(() => {
    console.log("Conectado ao mongo")
 }).catch((err) => {
    console.log("Erro ao se conectar ao mongo : " + err)
