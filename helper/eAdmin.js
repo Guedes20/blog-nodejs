ac21d8246dd3eaa6c27bd57bd52633e34f676fe9
Fernando Guedes <fernandoguedes20@gmail.com>
2019-12-24 11:03:14 -0500

    Correção na rota /404.

--- helper/eAdmin.js
+++ helper/eAdmin.js
@@ -3,7 +3,6 @@ module.exports = {
         if(req.isAuthenticated() && req.user.eAdmin == 1){
             return next();
         }
-
         req.flash("error_msg", "Você precisa ser admin para acessar esta pagina." )
         res.redirect("/")
 
